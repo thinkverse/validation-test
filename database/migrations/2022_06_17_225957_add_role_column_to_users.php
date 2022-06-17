643b4de229b1dc21475d7519a36ec9eaff359d97
thinkverse <hallberg.kim@gmail.com>
2022-06-18 01:25:05 +0200

    Fix role migration

--- database/migrations/2022_06_17_225957_add_role_column_to_users.php
+++ database/migrations/2022_06_17_225957_add_role_column_to_users.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('rule')->nullable();
+            $table->string('role')->nullable();
         });
     }
 
@@ -26,7 +26,7 @@ return new class extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('rule');
+            $table->dropColumn('role');
         });
     }
 };
