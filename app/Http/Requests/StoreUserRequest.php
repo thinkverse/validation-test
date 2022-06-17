25a313f0923ce604cd7e8fde815dbfa53634e6b5
thinkverse <hallberg.kim@gmail.com>
2022-06-18 01:02:36 +0200

    Add StoreUserRequest

---
+++ app/Http/Requests/StoreUserRequest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'role' => 'required',
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|confirmed',
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'role.required' => 'A role is required',
+            'name.required' => 'A name is required',
+            'email.email' => 'The email is not in a valid format',
+            'email.unique' => 'There is already a user with that email address',
+            'password.required' => 'A password is required',
+            'password.confirmed' => 'Your passwords dont match',
+        ];
+    }
+}
