6b5747cdfac686c921da38682d79fda3478e5382
thinkverse <hallberg.kim@gmail.com>
2022-06-20 13:13:44 +0200

    Fix user creation view

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -23,7 +23,7 @@
                         </div>
 
                         <!-- Username -->
-                        <div>
+                        <div class="mt-4">
                             <x-label for="username" :value="__('Username')" />
 
                             <x-input id="username" class="block mt-1 w-full" type="text" name="username" :value="old('username')" required />
